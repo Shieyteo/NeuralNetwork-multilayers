5407ce4dc6a20cd00f437ea9bbf9e8b6230a8832
sheyteo <nyce0979@gmail.com>
2022-08-19 04:39:49 +0200

    stable version tanh 3 middle neuron

--- NeuralNetwork multilayers/main.cpp
+++ NeuralNetwork multilayers/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <windows.h>
 
 double Sigmoid(double inp)
 {
@@ -16,6 +17,16 @@ double derivative_Error(double a, double b)
 double derivative_Sigmoid(double b)
 {
 	return b * (1 - b);
+}
+
+double m_max(double a, double b)
+{
+	return a > b ? a : b;
+}
+
+double m_min(double a, double b)
+{
+	return a < b ? a : b;
 }
 
 double derivative_tanh(double b)
@@ -47,23 +58,23 @@ std::vector<double> forwardProp(std::vector<std::vector<double>>* weights, std::
 }
 
 // input at index 0 and 1 expected output at index 2
-const std::vector<std::vector<double>> train_and_test_samples = { {1,0, 1},{1,1,0} ,{0,1,1} ,{0,0,0}}; //XOR Problem (Non linear)
+const std::vector<std::vector<double>> train_and_test_samples = { {1,0, 1},{1,1,0} ,{0,1,1},{0,0,0} }; //XOR Problem (Non linear) 
 
 const char* CHARMAP = "@$B%8&WM#oahkbdpqwmZO0QLCJUYXzcvunxrjftlI/|()1{}[]<>i!?*+~-;:,_^`'.";
 
 int main()
 {
 	srand(time(NULL)); // intialize rand
-	const double lr = 0.2;	//learning rate
+	const double lr = 0.1;	//learning rate
 
 	//initiate weights and biases
-	std::vector<std::vector<double>> weights1 = { { getRand(),getRand() },{ getRand(),getRand()} }; // from input layer to hidden layer ,{ getRand(),getRand()}
-	std::vector<std::vector<double>> weights2 = { { getRand()},{getRand() } }; // from hidden layer to output layer ,{ getRand()}
+	std::vector<std::vector<double>> weights1 = { { getRand(),getRand(),getRand() },{ getRand(),getRand(),getRand()} }; // from input layer to hidden layer 
+	std::vector<std::vector<double>> weights2 = { { getRand()},{getRand() },{ getRand()} }; // from hidden layer to output layer
 	std::vector<double> bias1 = { 1,1,1 }; // biases for  the hidden layer
 	std::vector<double> bias2 = { 1 }; // bias for  the output layer
 	
 
-	for (int epoch = 0; epoch < 100000; epoch++)
+	for (int epoch = 0; epoch < 100000000; epoch++)
 	{
 		double error = 0;
 		for (std::vector<double> sample : train_and_test_samples)
@@ -88,21 +99,22 @@ int main()
 			//Hiddenlayer error calculation										
 			double d_err_hidden0 = complete_error * weights2[0][0];
 			double d_err_hidden1 = complete_error * weights2[1][0];
-			//double d_err_hidden2 = complete_error * weights2[2][0];
+			double d_err_hidden2 = complete_error * weights2[2][0];
 
 
 			//double d_sig_hidden0 = derivative_Sigmoid(hidden_layer_values[0]);
 			//double d_sig_hidden1 = derivative_Sigmoid(hidden_layer_values[1]);
 			double d_sig_hidden0 = derivative_tanh(hidden_layer_values[0]);
 			double d_sig_hidden1 = derivative_tanh(hidden_layer_values[1]);
-			//double d_sig_hidden2 = derivative_tanh(hidden_layer_values[2]);
+			double d_sig_hidden2 = derivative_tanh(hidden_layer_values[2]);
 
 			//bias adjustment
 			bias2[0] -= lr * complete_error;
 
 			//weight adjustment
-			weights2[0][0] -= lr * complete_error * input[0];
-			weights2[1][0] -= lr * complete_error * input[1];
+			//std::cout << -log((1 - hidden_layer_values[0]) / hidden_layer_values[0]) << "\n";
+			weights2[0][0] -= lr * complete_error * hidden_layer_values[0];
+			weights2[1][0] -= lr * complete_error * hidden_layer_values[1];
 
 			//bias adjustment
 			bias1[0] -= lr * d_err_hidden0 * d_sig_hidden0;
@@ -113,25 +125,27 @@ int main()
 			weights1[1][0] -= lr * d_err_hidden0 * d_sig_hidden0 * input[1];
 			weights1[0][1] -= lr * d_err_hidden1 * d_sig_hidden1 * input[0];
 			weights1[1][1] -= lr * d_err_hidden1 * d_sig_hidden1 * input[1];
+			weights1[0][2] -= lr * d_err_hidden2 * d_sig_hidden2 * input[0];
+			weights1[1][2] -= lr * d_err_hidden2 * d_sig_hidden2 * input[1];
 			error += abs(expected - output_layer_values[0]);
 		}
 		std::string map;
 		int size = 30;
-		
+		for (float i = 0; i < size; i++)
+		{
+			for (float j = 0; j < size; j++)
+			{
+				std::vector<double> val_input = { i / size,j / size };
+				std::vector<double> hidden_output = forwardProp(&weights1, &val_input, &bias1);
+				std::vector<double> output = forwardProp(&weights2, &hidden_output, &bias2);
+				map += CHARMAP[int(m_max(0,int(output[0] * 68)-1))];
+				map += ' ';
+			}
+			map += "\n";
+		}
 		if (epoch % 100 == 0)
 		{
-			for (float i = 0; i < size; i++)
-			{
-				for (float j = 0; j < size; j++)
-				{
-					std::vector<double> val_input = { i / size,j / size };
-					std::vector<double> hidden_output = forwardProp(&weights1, &val_input, &bias1);
-					std::vector<double> output = forwardProp(&weights2, &hidden_output, &bias2);
-					map += CHARMAP[int(output[0] * 67)];
-					map += ' ';
-				}
-				map += "\n";
-			}
+			Sleep(300);
 			system("cls");
 			std::cout << "Epoch: "<<epoch<<"\nError: "<<(error / 4) << "\n" << map;
 		}
